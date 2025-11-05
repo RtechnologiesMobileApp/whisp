d01bb9d463090a81711cad2b94499eb36e180a23
MDK-404 <mdk968196@gmail.com>
2025-11-05 19:41:10 +0500

    .

--- lib/features/auth/view/dob_screen.dart
+++ lib/features/auth/view/dob_screen.dart
@@ -1,102 +1,12 @@
-// import 'package:flutter/cupertino.dart';
-// import 'package:flutter/material.dart';
-// import 'package:flutter_cupertino_date_picker_fork/flutter_cupertino_date_picker_fork.dart';
-// import 'package:get/get.dart';
-// import 'package:whisp/config/constants/colors.dart';
-// import 'package:whisp/core/widgets/custom_button.dart';
-// import 'package:whisp/core/widgets/custom_date_picker.dart';
-// import 'package:whisp/features/auth/controllers/dob_controller.dart';
- 
-
-// class DobScreen extends StatelessWidget {
-//   const DobScreen({super.key});
-
-//   @override
-//   Widget build(BuildContext context) {
-//     final controller = Get.put(DobController());
-
-//     return Scaffold(
-//       backgroundColor: Colors.white,
-//       appBar: AppBar(
-//         elevation: 0,
-//         backgroundColor: Colors.white,
-//         leading: IconButton(
-//           icon: const Icon(Icons.arrow_back_ios, color: Colors.black),
-//           onPressed: () => Get.back(),
-//         ),
-//       ),
-//       body: Padding(
-//         padding: const EdgeInsets.symmetric(horizontal: 24, vertical: 10),
-//         child: Column(
-//           crossAxisAlignment: CrossAxisAlignment.start,
-//           children: [
-//             // 🟣 Page Heading
-//             Text(
-//               "Date of Birth",
-//               style: TextStyle(
-//                 fontSize: 22,
-//                 fontWeight: FontWeight.w700,
-//                 color: Colors.black,
-//               ),
-//             ),
-//             const SizedBox(height: 6),
-
-//             // 🩶 Subtitle
-//             const Text(
-//               "Select your Date of Birth below",
-//               style: TextStyle(
-//                 fontSize: 14,
-//                 color: Colors.grey,
-//               ),
-//             ),
-
-//             const SizedBox(height: 20),
-
-//             // 📅 Cupertino Date Picker
- 
-//             SizedBox(
-//   height: 200,
-//   child: CupertinoTheme(
-//     data: CupertinoThemeData(
-//       brightness: Brightness.light,
-//       primaryColor: AppColors.primary, // 💜 sets highlight color
-//     ),
-//     child: CupertinoDatePicker(
-//       mode: CupertinoDatePickerMode.date,
-//       initialDateTime: DateTime(2000, 1, 1),
-//       maximumDate: DateTime.now(),
-//       minimumYear: 1900,
-//       onDateTimeChanged: (DateTime newDate) {
-//         controller.selectedDate.value = newDate;
-//       },
-//     ),
-//   ),
-// ),
-
-//             const Spacer(),
-
-//             // 🟣 Custom Continue Button
-//             Obx(() => CustomButton(
-//                   text: "Continue",
-//                   isLoading: controller.isLoading.value,
-//                   onPressed: () async {
-//                     await controller.saveDateOfBirth();
-//                   },
-//                 )),
-//             const SizedBox(height: 20),
-//           ],
-//         ),
-//       ),
-//     );
-//   }
-// }
-
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_cupertino_date_picker_fork/flutter_cupertino_date_picker_fork.dart';
 import 'package:get/get.dart';
 import 'package:whisp/config/constants/colors.dart';
 import 'package:whisp/core/widgets/custom_button.dart';
+import 'package:whisp/core/widgets/custom_date_picker.dart';
 import 'package:whisp/features/auth/controllers/dob_controller.dart';
+ 
 
 class DobScreen extends StatelessWidget {
   const DobScreen({super.key});
@@ -104,6 +14,7 @@ class DobScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     final controller = Get.put(DobController());
+
     return Scaffold(
       backgroundColor: Colors.white,
       appBar: AppBar(
@@ -129,6 +40,7 @@ class DobScreen extends StatelessWidget {
               ),
             ),
             const SizedBox(height: 6),
+
             // 🩶 Subtitle
             const Text(
               "Select your Date of Birth below",
@@ -137,40 +49,32 @@ class DobScreen extends StatelessWidget {
                 color: Colors.grey,
               ),
             ),
+
             const SizedBox(height: 20),
-            // 📅 Cupertino Date Picker with Primary Color
-            Container(
-              height: 200,
-              decoration: BoxDecoration(
-                border: Border(
-                  top: BorderSide(color: Colors.grey.shade300, width: 0.5),
-                  bottom: BorderSide(color: Colors.grey.shade300, width: 0.5),
-                ),
-              ),
-              child: CupertinoTheme(
-                data: CupertinoThemeData(
-                  brightness: Brightness.light,
-                  primaryColor: AppColors.primary,
-                  textTheme: CupertinoTextThemeData(
-                    dateTimePickerTextStyle: TextStyle(
-                      color: AppColors.primary, // Selected text color
-                      fontSize: 22,
-                    ),
-                  ),
-                ),
-                child: CupertinoDatePicker(
-                  mode: CupertinoDatePickerMode.date,
-                  initialDateTime: DateTime(2000, 1, 1),
-                  maximumDate: DateTime.now(),
-                  minimumYear: 1900,
-                  backgroundColor: Colors.white,
-                  onDateTimeChanged: (DateTime newDate) {
-                    controller.selectedDate.value = newDate;
-                  },
-                ),
-              ),
-            ),
+
+            // 📅 Cupertino Date Picker
+ 
+            SizedBox(
+  height: 200,
+  child: CupertinoTheme(
+    data: CupertinoThemeData(
+      brightness: Brightness.light,
+      primaryColor: AppColors.primary, // 💜 sets highlight color
+    ),
+    child: CupertinoDatePicker(
+      mode: CupertinoDatePickerMode.date,
+      initialDateTime: DateTime(2000, 1, 1),
+      maximumDate: DateTime.now(),
+      minimumYear: 1900,
+      onDateTimeChanged: (DateTime newDate) {
+        controller.selectedDate.value = newDate;
+      },
+    ),
+  ),
+),
+
             const Spacer(),
+
             // 🟣 Custom Continue Button
             Obx(() => CustomButton(
                   text: "Continue",
@@ -186,3 +90,5 @@ class DobScreen extends StatelessWidget {
     );
   }
 }
+
+ 
