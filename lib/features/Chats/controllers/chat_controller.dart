62de39e007af213e18921de6854e16617d031de5
MDK-404 <mdk968196@gmail.com>
2025-11-06 16:43:37 +0500

    .

---
+++ lib/features/Chats/controllers/chat_controller.dart
@@ -0,0 +1,27 @@
+import 'package:flutter/material.dart';
+import 'package:get/get.dart';
+import 'package:whisp/services/socket_service.dart';
+
+class ChatController extends GetxController {
+  final SocketService socketService = Get.find<SocketService>();
+  final TextEditingController messageController = TextEditingController();
+  RxList<Map<String, dynamic>> messages = <Map<String, dynamic>>[].obs;
+
+  @override
+  void onInit() {
+    super.onInit();
+   SocketService.to.onMessage((Map data) {
+  final message = Map<String, dynamic>.from(data);
+  messages.add(message);
+});
+
+  }
+
+  void sendMessage() {
+    final text = messageController.text.trim();
+    if (text.isEmpty) return;
+    socketService.sendMessage(text);
+    messages.add({"fromMe": true, "message": text});
+    messageController.clear();
+  }
+}
