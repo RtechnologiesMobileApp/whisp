aab0b389de7d4d4b3e1a06d52877d6b83e5cdaef
MDK-404 <mdk968196@gmail.com>
2025-11-21 15:16:35 +0500

    .

---
+++ lib/features/Chats/widgets/recording_bubble.dart
@@ -0,0 +1,78 @@
+import 'package:flutter/material.dart';
+
+class RecordingBubble extends StatelessWidget {
+  const RecordingBubble({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Align(
+      alignment: Alignment.centerLeft,
+      child: Container(
+        padding: const EdgeInsets.symmetric(horizontal: 14, vertical: 10),
+        margin: const EdgeInsets.symmetric(vertical: 4, horizontal: 8),
+        decoration: BoxDecoration(
+          color: Colors.white,
+          border: Border.all(color: Colors.grey.shade300),
+          borderRadius: const BorderRadius.only(
+            topLeft: Radius.circular(16),
+            topRight: Radius.circular(16),
+            bottomRight: Radius.circular(16),
+          ),
+        ),
+        child: Row(
+          mainAxisSize: MainAxisSize.min,
+          children: const [
+            PulseMic(), // 🔥 Animated Mic icon
+          ],
+        ),
+      ),
+    );
+  }
+}
+
+class PulseMic extends StatefulWidget {
+  const PulseMic({super.key});
+
+  @override
+  State<PulseMic> createState() => _PulseMicState();
+}
+
+class _PulseMicState extends State<PulseMic>
+    with SingleTickerProviderStateMixin {
+  late AnimationController _controller;
+  late Animation<double> _scaleAnim;
+
+  @override
+  void initState() {
+    super.initState();
+    _controller = AnimationController(
+      vsync: this,
+      duration: const Duration(milliseconds: 800),
+    )..repeat(reverse: true);
+
+    _scaleAnim = Tween<double>(begin: 0.85, end: 1.2).animate(
+      CurvedAnimation(
+        parent: _controller,
+        curve: Curves.easeInOut,
+      ),
+    );
+  }
+
+  @override
+  void dispose() {
+    _controller.dispose();
+    super.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return ScaleTransition(
+      scale: _scaleAnim,
+      child: const Icon(
+        Icons.mic,
+        color: Colors.red,
+        size: 18,
+      ),
+    );
+  }
+}
